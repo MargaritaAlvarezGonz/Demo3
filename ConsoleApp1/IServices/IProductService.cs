018dd6eaa32f7366225e6c584de89d44bc007982
Margarita Álvarez González <karessibienestar@gmail.com>
2023-01-18 00:12:32 +0100

    Add project files.

---
+++ ConsoleApp1/IServices/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.IServices
+{
+    public interface IProductService
+    {
+        int CreateProduct(Product product);
+        void UpdateProduct(Product product);
+        void DeleteProduct(int id);
+        List<Product> GetAllProducts();
+
+    }
+}
